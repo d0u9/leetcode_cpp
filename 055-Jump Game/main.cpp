a8823acbb82bdd694d7dc28f18c3b0397c2a3d68
Douglas <su_zibin@163.com>
2015-04-24 16:56:25 +0800

    055-X-Jump Game

---
+++ 055-Jump Game/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  leetcode-0055
+//
+//  Created by Douglas Su on 4/22/15.
+//  Copyright (c) 2015 Douglas Su. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = 1;
+        for (int i = 0; i < reach && reach < nums.size(); ++i)
+            reach = max(reach, i + 1 + nums[i]);
+        return reach >= nums.size();
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    vector<int> input = {2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6};
+    
+    Solution *so = new Solution;
+    auto re = so->canJump(input);
+    
+    cout << re << endl;
+    
+    return 0;
+}
